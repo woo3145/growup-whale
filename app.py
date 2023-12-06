c379df6fd6074cfe582432c1d60e858a39a01b8e
jennaaaaaaaaa <ggm4954@gmail.com>
2023-12-06 22:49:57 +0900

    수정

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import relationship
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 import secrets
-from services import loginService
+from services import loginService, registerService
 
 app = Flask(__name__)
 
