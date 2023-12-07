b6cf6f6db11fa40e78c6bdc27587ad9df2c0040b
woo3145 <lcwoo3145@gmail.com>
2023-12-07 10:20:34 +0900

    정적 데이터 추가(고래, 필요 경험치)

---
+++ services/dataService.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+
+def loadRequiredExp(app):
+    json_url = os.path.join(app.static_folder, 'data/required_exp.json')
+    with open(json_url, 'r') as json_file:
+        data = json.load(json_file)
+    return data
+
+
+def loadWhaleData(app):
+    json_url = os.path.join(app.static_folder, 'data/whale_data.json')
+    with open(json_url, 'r') as json_file:
+        data = json.load(json_file)
+    return data
