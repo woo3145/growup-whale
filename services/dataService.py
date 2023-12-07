6f1ed1db4e09e3f4af2a347244b421a8778c1340
woo3145 <lcwoo3145@gmail.com>
2023-12-07 15:15:26 +0900

    Python open 함수 인코딩문제 해결(윈도우/맥)

--- services/dataService.py
+++ services/dataService.py
@@ -4,13 +4,13 @@ import os
 
 def loadRequiredExp(app):
     json_url = os.path.join(app.static_folder, 'data/required_exp.json')
-    with open(json_url, 'r') as json_file:
+    with open(json_url, 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
     return data
 
 
 def loadWhaleData(app):
     json_url = os.path.join(app.static_folder, 'data/whale_data.json')
-    with open(json_url, 'r') as json_file:
+    with open(json_url, 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
     return data
