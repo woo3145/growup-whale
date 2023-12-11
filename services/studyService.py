05134e611a8fc1f5eb93a241013cb4092b8d9805
js1171 <bhd1171@naver.com>
2023-12-11 20:52:05 +0900

    레벨업때만 고래 이미지 변경

--- services/studyService.py
+++ services/studyService.py
@@ -9,14 +9,14 @@ def studyCheck(db, User, required_exp, studyType, user_email) :
     email = user_email
     user_list = db.session.query(User).filter_by(email=email).first()
 
-    # 한 번만 클릭 할 수 있도록 제어s
+    # # 한 번만 클릭 할 수 있도록 제어s
     user_date = user_list.starttime.date() if user_list.starttime else None
     cur_time = get_time()
 
     if cur_time == user_date :
         return {'success': False, 'message': '하루에 한 번만 누를 수 있음!'}
     
-    # 오늘 날짜 기록
+    # # 오늘 날짜 기록
     user_list.starttime = cur_time
     db.session.add(user_list)
     db.session.commit()
@@ -52,13 +52,13 @@ def studyCheck(db, User, required_exp, studyType, user_email) :
     curExp = 0 if not user_list.whale.exp else user_list.whale.exp
     total_exp = curExp + 1
     level = int(user_list.whale.level)
+    whale_list = user_list.whale
     if(required_exp <= total_exp):
         level += 1
+        whale_list.job = job
 
     # job, 경험치, 레벨 DB에 기록 (whale -> level, exp)
-    whale_list = user_list.whale
     whale_list.level = level
-    whale_list.job = job
     whale_list.exp = total_exp
     db.session.add(whale_list)
     db.session.commit()
